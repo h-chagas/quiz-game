0624c2442659d0c61cf8c76d68d33a033192cde4
h-chagas <hugochagasuk@gmail.com>
2022-09-13 08:32:43 +0100

    create js file to keep quiz questions apart

---
+++ questions.js
@@ -0,0 +1,54 @@
+// QUESTIONS
+
+export const myQuestions = [
+    {
+        "Question 01/05": "After how many Year's FIFA World Cup is held?",
+        "Answers:": {
+            a: "a) 2 Years",
+            b: "b) 3 Years",
+            c: "c) 4 Years",
+            d: "d) Every Year",
+        },
+        correctAnswer: "alt_c"
+    },
+    {
+        "Question 02/05": "Which Country won the first FIFA World Cup?",
+        "Answers:": {
+            a: "a) Argentina",
+            b: "b) Uruguay",
+            c: "c) Italy",
+            d: "d) Brazil",
+        },
+        correctAnswer: "alt_b"
+    },
+    {
+        "Question 03/05": "Who won the first ICC World Cup?",
+        "Answers:": {
+            a: "a) India",
+            b: "b) West Indies",
+            c: "c) England",
+            d: "d) Australia",
+        },
+        correctAnswer: "alt_b"
+    },
+    {
+        "Question 04/05": "Who won the first T20 World Cup?",
+        "Answers:": {
+            a: "a) Pakistan",
+            b: "b) India",
+            c: "c) Sri Lanka",
+            d: "d) West Indies",
+        },
+        correctAnswer: "alt_b"
+    },
+    {
+        "Question 05/05": "Who is known as the Flying Sikh?",
+        "Answers:": {
+            a: "a) Michael Johnson",
+            b: "b) Usain Bolt",
+            c: "c) Milkha Sing",
+            d: "d) Carl Lewis",
+        },
+        correctAnswer: "alt_c"
+    },
+]
